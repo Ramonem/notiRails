ef8e4d9e7bd3a285270bfb1141a9c2e5ad3c9d79
Ramonem <ramon.3scobar@gmail.com>
2016-06-17 23:06:02 -0300

    limit 10

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :destroy, :update]
   def index
-    @posts = Post.all.where("created_at >= ?", 1.week.ago).order('visit DESC')
+    @posts = Post.all.where("created_at >= ?", 1.week.ago).order('visit DESC').limit(10)
     @popular = Post.all.order('visit DESC').limit(10)
     @lastest = Post.all.order('created_at DESC')
     @categories = Category.all
